bbf3ca423d0b04ff2e330d2cdb819fcbdb6fbc39
Darwin Rojas <josteinrojas590@gmail.com>
2025-02-13 19:19:22 -0500

    Agregando mas carpetas y avanzado el proyecto cortez cms

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navabar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     //Creamos un div con la clase app y Agregamos el componente Navabar
     <div className='app'>
       <Navabar/>
+      <Routes>
+        {/* Creamos una ruta con el path / y el componente Home */}
+        <Route path='/' element={<Home/>}/>
+        {/* Creamos una ruta con el path /cart y el componente Cart */}
+        <Route path='/cart' element={<Cart/>}/>
+        {/* Creamos una ruta con el path /placeorder y el componente PlaceOrder */}
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
