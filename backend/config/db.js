5667f6808e5cb5fb859bcfdebe8f5416e44ebea1
Jeferson14 <je144cc@gmail.com>
2025-02-19 14:48:59 -0500

    Eliminar config/db.js del repositorio y actualizar .gitignore

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://je144cc:doraemon123456@cluster0.wqyzr.mongodb.net/julia-fish"
-    )
-    .then(() => console.log("BD Connected"));
-	//me quede en 3.45
-};
