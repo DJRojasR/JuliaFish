8fecfb0b5269281d3fa189d5417db28a15db6425
Jeferson14 <je144cc@gmail.com>
2025-03-13 21:42:29 -0500

    Actualizando proyecto

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import "./Add.css"
+import React from "react";
+import "./Add.css";
+import { assets } from "../../assets/assets";
 
 const Add = () => {
   return (
-	<div>Add version asdsdfsfdsfddsffd</div>
-  )
-} 
+    <div>
+      <div className="add">
+        <form className="flex-col">
+          <div className="add-img-upload flex-col">
+            <p>Subir imagen</p>
+            <label htmlFor="image">
+              <img src={assets.upload} alt="" />
+            </label>
+            <input type="file" id="image" hidden required />
+          </div>
+          <div className="add-product-name flex-col">
+            <p>Nombre del producto</p>
+            <input type="text" name="name" placeholder="Escribe aquí" />
+          </div>
+          <div className="add-product-description flex-col">
+            <p>Descripción del producto</p>
+            <textarea
+              name="description"
+              rows="6"
+              placeholder="Escribe aquí"
+              required
+            ></textarea>
+          </div>
+          <div className="add-category-price">
+            <div className="add-category flex-col">
+              <p>Categoría</p>
+              <select name="category" required>
+                <option value="Ceviche">Ceviche</option>
+                <option value="Chaufa">Chaufa</option>
+                <option value="Sopas">Sopas</option>
+                <option value="Causas">Causas</option>
+                <option value="Chicharrones">Chicharrones</option>
+                <option value="Cocteles">Cocteles</option>
+                <option value="Arroces">Arroces</option>
+                <option value="Bebidas">Bebidas</option>
+              </select>
+            </div>
+            <div className="add-price flex-col">
+              <p>Price</p>
+              <input
+                type="number"
+                name="price"
+                placeholder="Escribe aquí"
+                required
+              />
+            </div>
+          </div>
 
-export default Add
+          <button type="submit" className="add-btn">
+            Agregar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
