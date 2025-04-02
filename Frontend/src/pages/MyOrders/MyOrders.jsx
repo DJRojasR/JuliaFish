5422bca01b86816aa72c6c59c593493dedb2a960
Jeferson14 <je144cc@gmail.com>
2025-04-02 10:55:24 -0500

    hasta 9:00

--- Frontend/src/pages/MyOrders/MyOrders.jsx
+++ Frontend/src/pages/MyOrders/MyOrders.jsx
@@ -3,31 +3,42 @@ import "./MyOrders.css";
 import { StoreContext } from "../../context/StoreContext";
 import { useState } from "react";
 import axios from "axios";
-
+import { assets } from "../../assets/assets";
 
 const MyOrders = () => {
   const { url, token } = useContext(StoreContext);
   const [data, setData] = useState([]);
   const fetchOrders = async () => {
     const response = await axios.post(
-      url+"/api/order/userorders",
+      url + "/api/order/userorders",
       {},
       { headers: { token } }
-    ); 
-	setData(response.data.data);
-	console.log(response.data.data);
+    );
+    setData(response.data.data);
+    console.log(response.data.data);
   };
 
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+  }, [token]);
 
-  useEffect(()=>{
-	if(token){
-		fetchOrders();
-
-	}
-
-  },[token])
-
-  return <div>MyOrdersdfdf</div>;
+  return (
+    <div className="myorders">
+      <h2>My orders</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="ZZZZ" />
+              <p></p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
